3627ac7e197d4f78bb577dd391bb7b566efa06e1
Vitor Aguiar <joaovitor4guiar@gmail.com>
2026-01-12 14:38:20 -0300

    feat: add Transaction entity

---
+++ api/HomeFinance.Core/Exceptions/DomainException.cs
@@ -0,0 +1,3 @@
+namespace HomeFinance.Core.Exceptions;
+
+public abstract class DomainException(string message) : Exception(message);
---
+++ api/HomeFinance.Core/Exceptions/UnderageIncomeNotAllowedException.cs
@@ -0,0 +1,3 @@
+namespace HomeFinance.Core.Exceptions;
+
+public class UnderageIncomeNotAllowedException() : DomainException("Underage persons cannot have income transactions.");
