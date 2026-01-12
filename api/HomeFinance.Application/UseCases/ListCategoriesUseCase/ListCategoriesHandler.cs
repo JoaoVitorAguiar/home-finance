53ec54f5b24b884aad90dc657045988daea8be61
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2026-01-12 10:09:20 -0300

    Feat: add list categories use case and related components

---
+++ api/HomeFinance.Application/UseCases/ListCategoriesUseCase/ListCategoriesHandler.cs
@@ -0,0 +1,22 @@
+using HomeFinance.Core.Repositories;
+
+namespace HomeFinance.Application.UseCases.ListCategoriesUseCase;
+
+public static class ListCategoriesHandler
+{
+    public static async Task<List<ListCategoriesResponse>> Handle(
+        ListCategoriesQuery command,
+        ICategoryRepository categoryRepository
+    )
+    {
+        var categories = await categoryRepository.GetAllAsync();
+
+        return categories
+            .Select(c => new ListCategoriesResponse(
+                c.Id,
+                c.Description,
+                c.Purpose.ToString()
+            ))
+            .ToList();
+    }
+}
