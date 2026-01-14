f776b4994525abfe09effb88830da5f6d56ddced
Vitor Aguiar <joaovitor4guiar@gmail.com>
2026-01-14 07:35:05 -0300

    feat: add PeopleService

---
+++ web-app/src/services/people.service.ts
@@ -0,0 +1,21 @@
+import { api } from "@/services/api"
+import type { Person } from "@/types/person"
+import type { CreatePersonInput } from "@/types/create-person.input.tscreate-person.input"
+
+
+
+export class PeopleService {
+    static async getAll(): Promise<Person[]> {
+        const { data } = await api.get<Person[]>("/persons")
+        return data
+    }
+
+    static async create(input: CreatePersonInput): Promise<number> {
+        const { data } = await api.post<{ id: number }>("/persons", input)
+        return data.id
+    }
+
+    static async delete(id: number): Promise<void> {
+        await api.delete(`/persons/${id}`)
+    }
+}
